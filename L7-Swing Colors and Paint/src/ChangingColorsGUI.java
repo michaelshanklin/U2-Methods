8273a6b8041584a5971d3d5d6622462e0bf32f3f
Michael Shanklin <shankmic002@warren.k12.in.us>
2021-10-22 11:42:43 -0400

    ColorSwitchGUI

--- L7-Swing Colors and Paint/src/ChangingColorsGUI.java
+++ L7-Swing Colors and Paint/src/ChangingColorsGUI.java
@@ -26,9 +26,20 @@ public class ChangingColorsGUI {
         panel = new JPanel();
         colorClicker = new JButton("Change the Colors");
 
+
+
         //create a new Custom color
+        //see globals
+
+
+        Color panelBackground = new Color(33, 33, 33);
+        Color buttonForeground = new Color (109, 152, 134);
+        Color buttonBackground = new Color(217, 202, 179);
 
         //Set the colors of our buttons and panel
+        panel.setBackground(panelBackground);
+        colorClicker.setForeground(buttonForeground);
+        colorClicker.setBackground(buttonBackground);
 
         panel.add(colorClicker);
         window.add(panel);
@@ -37,7 +48,12 @@ public class ChangingColorsGUI {
     }
 
     //Add a listener to change the color when the button is clicked
+    private class ColorChanger implements ActionListener{
 
+        public void actionPerformed(ActionEvent actionEvent);
+        colorClicker.setForeground(buttonBackground);
+        colorClicker.setBackground(panelBackground);
+    }
 
 
 
