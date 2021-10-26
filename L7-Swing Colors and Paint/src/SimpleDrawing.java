7be5ba212a1877760abe5fbd43c7e5d094ad7d36
Michael Shanklin <shankmic002@warren.k12.in.us>
2021-10-26 13:25:05 -0400

    ColorSwitchGUI

--- L7-Swing Colors and Paint/src/SimpleDrawing.java
+++ L7-Swing Colors and Paint/src/SimpleDrawing.java
@@ -1,6 +1,40 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class SimpleDrawing {
 
     public static void main(String[] args) {
+        new SimpleDrawingGUI();
+        frame = new JFrame("House");
+        frame.setSize(400, 600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+
+        //Add the panel to the frame
+        JPanel House = new drawingHouse();
+
+        frame.add(panel);
+        frame.setVisible(true);
+    }
+
+    private class DrawingHouse extends JPanel {
+        //Set background color
+        setBackground(Color.yellow);
+
+    }
+
+    public void paintComponent(Graphics g) {
+
+        super.paintComponent(g);
+
+        g.setColor(Color.ORANGE);
+        g.fillRect(5, 234, 255, 1);
+
+        g.setColor(Color.YELLOW);
+        g.fillOval(255, 222, 5, 0);
+
+        g.setColor(Color.RED);
+        g.fillRect(204, 51, 0, 0);
 
     }
 }
